c317b18f0f4e505f2ca13a076bf490ffa10aae19
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2018-11-10 23:37:32 +0600

    Create 1124 - Celebrity jeopardy.cpp

---
+++ 1124 - Celebrity jeopardy.cpp
@@ -0,0 +1,20 @@
+/***
+**     Author: Khairul Anam Mubin
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char str[1000];
+
+    while(scanf(" %[^\n]",&str)==1);
+    {
+        printf("%s\n",a);
+    }
+    return 0;
+}
