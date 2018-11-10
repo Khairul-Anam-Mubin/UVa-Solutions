3bdaa9b2dc4c6d4478d288ef83763998d588e1dc
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2018-11-11 01:00:33 +0600

    Create 10970 - Big Chocolate.cpp

---
+++ 10970 - Big Chocolate.cpp
@@ -0,0 +1,20 @@
+/***
+**     Author: Khairul Anam Mubin
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int m,n;
+
+    while(scanf("%d%d",&m,&n)==2)
+    {
+        printf("%d\n",m*n-1);
+    }
+    return 0;
+}
