9ac4c73e601e77ee524c5ffe78ecb922e1af8d2d
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2018-11-11 00:36:36 +0600

    Create 10469 - To Carry or not to Carry.cpp

---
+++ 10469 - To Carry or not to Carry.cpp
@@ -0,0 +1,17 @@
+/***
+**     Author: Khairul Anam Mubin
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+
+using namespace std;
+
+int main()
+{
+    long int a,b;
+    while(scanf("%ld%ld",&a,&b)==2)
+    {
+        cout<<(a^b)<<endl;
+    }
+    return 0;
+}
