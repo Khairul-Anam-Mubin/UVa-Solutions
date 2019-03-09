54b72dda120ad6be3868be4ed8d93f8bd9962e38
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-03-09 18:23:39 +0600

    Create 10523 - Very Easy !!!.cpp

---
+++ 10523 - Very Easy !!!.cpp
@@ -0,0 +1,131 @@
+/***
+**     Author: Khairul Anam Mubin
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm>
+
+#define pb push_back
+const double pi = acos(-1.0);
+//typedef long long ll;
+//typedef unsigned long long ull;
+
+using namespace std;
+
+string BigintSum(string str1,string str2);
+string BigintProd(string str1,string str2);
+
+int main()
+{
+    int n,a;
+
+    while(cin >> n >> a)
+    {
+        ostringstream str1;
+        str1 << a;
+        string geek = str1.str();
+        string prod,prod2,sum;
+        //cout << geek;
+        //cout << geek.size();
+        for(int i=1;i<=n;i++){
+            ostringstream str2;
+            str2 << i;
+            string loop = str2.str();
+
+            if(i==1){
+                prod=geek;
+            }
+            else{
+                prod = BigintProd(prod,geek);
+                prod2 = BigintProd(prod,loop);
+            }
+
+            sum = BigintSum(sum,prod2); 
+        }
+        cout << BigintSum(sum,geek) << endl;
+
+    }
+    return 0;
+}
+string BigintProd(string str1,string str2)
+{
+    if(str1.size() < str2.size())
+        swap(str1,str2);
+    
+    int len1=(int)str1.size(); 
+    int len2=(int)str2.size();
+    int carry=0,cnt=1;
+    string prod,pro;
+
+    for(int i=len2-1; i>=0; i--){
+        carry=0;
+        for(int j=len1-1; j>=0; j--){
+            int x = (str2[i] - '0') * (str1[j] - '0') + carry;
+            carry=x/10;
+            x=x%10;
+            prod.push_back(x+'0');
+        }
+        if(carry)
+            prod.push_back(carry+'0');
+        reverse(prod.begin(),prod.end());
+        if(cnt==1){
+            pro=prod;
+            cnt++;
+            prod.clear();
+        }
+        else{
+            pro=BigintSum(pro,prod);
+            prod.clear();
+        }
+        for(int k=len2-1;k>=i;k--){
+            prod.push_back('0');
+        }
+    }
+    //return "0" if any multiply apply with 0
+    if(pro[0]=='0' && pro.size()>1)
+        pro="0";
+    return pro;
+}
+string BigintSum(string str1,string str2)
+{
+    if(str1.size() < str2.size())
+        swap(str1,str2);
+    
+    int len1=(int)str1.size(); 
+    int len2=(int)str2.size();
+    int n=len2;
+    int carry=0;
+    string sum;
+
+    for(int i=len1-1, j=len2-1; i>=0 ; i--, j--){
+        int x;
+        if(n){
+            x = str1[i] - '0' + str2[j] - '0' + carry;
+            n--;
+        }
+        else{
+            x = str1[i] - '0' + carry;
+        }
+        carry=x/10;
+        x=x%10;
+        sum.push_back(x+'0');
+    }
+    if(carry)
+        sum.push_back(carry+'0');
+    reverse(sum.begin(),sum.end());
+
+    return sum;
+}
