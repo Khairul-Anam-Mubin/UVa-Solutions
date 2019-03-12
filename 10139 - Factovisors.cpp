92b606c874ff8cff99bf60f6a02a3928cb9e1403
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-03-12 17:18:39 +0600

    Update 10139 - Factovisors.cpp

--- 10139 - Factovisors.cpp
+++ 10139 - Factovisors.cpp
@@ -1,74 +1,47 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+typedef long long ll;
 
-
-int main()
-{
-    int m,n,flag;
-
-    while(cin >> n >> m)
-    {
-        if(n>=m){
-            printf("%d divides %d!\n",m,n);
-            continue;
-        }
-        flag=1;
-        int sq=sqrt(m);
-        int x=m;
-        for(int i=2;i<=sq;i++){
-            if(x%i==0){
-                int cnt=0;
-                while(x%i==0){
-                    cnt++;
-                    x/=i;
-                }
-                int y=n;
-                int j=i;
-                int k=0;
-                int f=0;
-                while(true)
-                {
-                    k+=n/j;
-                    y=n/j;
-                    if(k==0){
-                        flag=0;
-                        break;
-                    }
-                    if(k>=cnt){
-                        f=1;
-                        break;
-                    }
-                    if(y==0){
-                        break;
-                    }
-                    j=j*i;
-                }
-                if(f==0){
-                    flag=0;
-                    break;
-                }
-                if(x==1){
-                    break;
-                }
-            }
-        }
-        if(x>1){
-            int s=n/x;
-            if(s==0){
-                flag=0;
-            }
-            else
-                flag=1;
-        }
-        if(flag==1){
-            printf("%d divides %d!\n",m,n);
-        }
-        else{
-            printf("%d does not divide %d!\n",m,n);
-        }
+bool check(ll val, ll div, ll cnt) {
+    ll totCnt = 0, d = div;
+    while(1) {
+        ll t = val/div;
+        if(t == 0)
+            break;
+        totCnt += t, div *= d;
     }
 
+    return totCnt >= cnt;
+}
 
-    return 0;
+int main() {
+	ll n, m;
+
+	while(scanf("%lld%lld", &n, &m) == 2) {
+		ll x = m;
+		bool found = 1;
+
+		for(ll i = 2; i * i <= x; ++i) {
+			ll cnt = 0;
+
+			while(x%i == 0)
+				x /= i, ++cnt;
+
+			if(cnt > 0 && !check(n, i, cnt)) { 
+				found = 0;
+				break;
+			}
+		}
+
+		if(x > 1 && !check(n, x, 1))
+			found = 0;
+		
+		if(found)
+			printf("%lld divides %lld!\n", m, n);
+		else
+			printf("%lld does not divide %lld!\n", m, n);
+
+	}
+
+	return 0;
 }
