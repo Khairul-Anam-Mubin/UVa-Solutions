8f4c6e14768c6f927105aa3ca386a8d3202b6338
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-03-18 13:07:42 +0600

    Create 10062 - Tell me the frequencies!.cpp

---
+++ 10062 - Tell me the frequencies!.cpp
@@ -0,0 +1,74 @@
+/***
+**     Author: Khairul Anam Mubin
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm>
+
+#define pb      push_back
+#define Sqr(n)  (n*n)
+#define Mex(v)  *max_element(v.begin(),v.end())
+#define Men(v)  *min_element(v.begin(),v.end())
+#define Fin     freopen("input.txt","r",stdin)
+#define Fout    freopen("output.txt","w",stdout)
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+const double pi = acos(-1.0);
+
+template <typename T> T Abs(T a) {if(a<0)return -a;else return a;}
+template <typename T> T BigMod (T b,T p,T m){if (p == 0) return 1;if (p%2 == 0){T s = BigMod(b,p/2,m);return ((s%m)*(s%m))%m;}return ((b%m)*(BigMod(b,p-1,m)%m))%m;}
+template <typename T> T Pow(T B,T P){ if(P==0) return 1; if(P&1) return B*Pow(B,P-1);  else return Sqr(Pow(B,P/2));}
+template <typename T> T gcd(T a,T b){if(a<0)return gcd(-a,b);if(b<0)return gcd(a,-b);return (b==0)?a:gcd(b,a%b);}
+template <typename T> T lcm(T a,T b) {if(a<0)return lcm(-a,b);if(b<0)return lcm(a,-b);return a*(b/gcd(a,b));}
+
+
+//Code starts from here......
+
+int main()
+{
+    string str;
+    int x = 0;
+    while(getline(cin,str))
+    {
+        if(x!=0)
+            printf("\n");
+
+        vector <int> v(130,0);
+        int maxx = 100000;
+
+        int len = str.size();
+        for(int i = 0 ; i<len ; ++i)
+            v[str[i]]++;
+        for(int i = 0 ; i<=129 ; ++i)
+            if(v[i] == 0)
+                v[i] = maxx;
+
+        for(int i = 128 ; i >= 32 ;i--){
+                int mn = Men(v);
+            for(int j = 128 ; j>=32 ; j--){
+                if(v[j]==mn &&  mn!=maxx ){
+                    cout << j << " " << mn << "\n";
+                    v[j] = maxx;
+                    break;
+                }
+            }
+        }
+        x++;
+    }
+    return 0;
+}
