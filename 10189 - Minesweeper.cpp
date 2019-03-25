6cf9bb81b44b4f1bfb968881a83bc9d48c4c9ad3
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-03-25 11:44:46 +0600

    Create 10189 - Minesweeper.cpp

---
+++ 10189 - Minesweeper.cpp
@@ -0,0 +1,111 @@
+/***
+**     Author: Khairul Anam Mubin
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm>
+
+#define pb      push_back
+#define Sqr(n)  (n*n)
+#define Mex(v)  *max_element(v.begin(),v.end())
+#define Men(v)  *min_element(v.begin(),v.end())
+#define Fin     freopen("input.txt","r",stdin)
+#define Fout    freopen("output.txt","w",stdout)
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+const double pi = acos(-1.0);
+
+template <typename T> T Abs(T a) {if(a<0)return -a;else return a;}
+template <typename T> T BigMod (T b,T p,T m){if (p == 0) return 1;if (p%2 == 0){T s = BigMod(b,p/2,m);return ((s%m)*(s%m))%m;}return ((b%m)*(BigMod(b,p-1,m)%m))%m;}
+template <typename T> T Pow(T B,T P){ if(P==0) return 1; if(P&1) return B*Pow(B,P-1);  else return Sqr(Pow(B,P/2));}
+template <typename T> T gcd(T a,T b){if(a<0)return gcd(-a,b);if(b<0)return gcd(a,-b);return (b==0)?a:gcd(b,a%b);}
+template <typename T> T lcm(T a,T b) {if(a<0)return lcm(-a,b);if(b<0)return lcm(a,-b);return a*(b/gcd(a,b));}
+
+//Code starts from here......
+
+int main()
+{
+	// File input and outputs : 	Fin ; Fout ;
+	
+	int n , m , tc = 0;
+
+	while(scanf("%d%d",&n,&m)==2)
+	{
+		if(n == 0 && m == 0) break;
+
+		if(tc !=0 )
+			printf("\n");
+
+		char ch ;
+		int matrix[n+100][m+100];
+
+		for(int i = 0 ; i <= n+1 ; i++)
+			for(int j = 0 ; j <= m+1 ; j++)
+				matrix[i][j] = 0;
+
+		for(int i = 0 ; i < n ; i++){
+			for(int j = 0 ; j < m ; j++){
+				cin >> ch;
+				if(ch == '*')
+					matrix[i+1][j+1] = -5; 
+			}
+		}
+
+		for(int i = 0 ; i <= n+1 ; i++){
+			for(int j = 0 ; j <= m+1 ; j++){
+				if(matrix[i][j] == -5){
+        
+					if(matrix[i][j+1] != -5)
+						matrix[i][j+1]++;
+
+					if(matrix[i][j-1] != -5)
+						matrix[i][j-1]++;
+
+					if(matrix[i-1][j] != -5)
+						matrix[i-1][j]++;
+
+					if(matrix[i-1][j+1] != -5)
+						matrix[i-1][j+1]++;
+
+					if(matrix[i-1][j-1] != -5)
+						matrix[i-1][j-1]++;
+
+					if(matrix[i+1][j] != -5)
+						matrix[i+1][j]++;
+
+					if(matrix[i+1][j+1] != -5)
+						matrix[i+1][j+1]++;
+
+					if(matrix[i+1][j-1] != -5)
+						matrix[i+1][j-1]++;
+				}
+			}
+		}
+		printf("Field #%d:\n",++tc);
+		for(int i = 1 ; i <= n ; i++){
+			for(int j = 1 ; j <= m ; j++){
+				if(matrix[i][j] == -5)
+					printf("*");
+				else
+					printf("%d",matrix[i][j]);
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
