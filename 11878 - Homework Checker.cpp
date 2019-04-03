0be2985959fd60a922fc8c04f57ba7d06fb7c6c6
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-04-03 17:14:28 +0600

    Create 11878 - Homework Checker.cpp

---
+++ 11878 - Homework Checker.cpp
@@ -0,0 +1,92 @@
+/***
+**     Author: Khairul Anam Mubin
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm>
+
+#define pb      push_back
+#define Sqr(n)  (n*n)
+#define Sort(v) sort(v.begin(),v.end())  
+#define Mxe(v)  *max_element(v.begin(),v.end())
+#define Mne(v)  *min_element(v.begin(),v.end())
+#define Fin     freopen("input.txt","r",stdin)
+#define Fout    freopen("output.txt","w",stdout)
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+const double pi = acos(-1.0);
+
+template <typename T> T Abs(T a) {if(a<0)return -a;else return a;}
+template <typename T> T BigMod (T b,T p,T m){if (p == 0) return 1;if (p%2 == 0){T s = BigMod(b,p/2,m);return ((s%m)*(s%m))%m;}return ((b%m)*(BigMod(b,p-1,m)%m))%m;}
+template <typename T> T Pow(T B,T P){ if(P==0) return 1; if(P&1) return B*Pow(B,P-1);  else return Sqr(Pow(B,P/2));}
+template <typename T> T gcd(T a,T b){if(a<0)return gcd(-a,b);if(b<0)return gcd(a,-b);return (b==0)?a:gcd(b,a%b);}
+template <typename T> T lcm(T a,T b) {if(a<0)return lcm(-a,b);if(b<0)return lcm(a,-b);return a*(b/gcd(a,b));}
+
+char uplowch(char ch){if(ch >= 'A' &&  ch <= 'Z') ch += 32; return ch;}
+char lowupch(char ch){if(ch >= 'a' &&  ch <= 'z') ch -= 32; return ch;}
+
+#define MOD 1000000007
+#define MAX -1000000007
+#define MIN 1000000007
+
+//Code starts from here......
+
+int main()
+{
+    // File input and outputs :   Fin ; Fout ;
+
+    string str ;
+    int cnt = 0;
+
+    while(cin >> str)
+    {
+    	int len = str.size();
+    	int i = 0 ;
+    	int x = 0;
+
+    	while(str[i] <= '9' && str[i] >='0'){
+    		x = x * 10 + (str[i] - '0');
+    		i++;
+    	}
+
+    	char op = str[i];
+    	i++;
+    	int y = 0;
+    	
+    	while(str[i] <= '9' && str[i] >='0'){
+    		y = y * 10 + (str[i] - '0');
+    		i++;
+    	}
+    	
+    	i++;
+    	int ans = 0;
+    	
+    	while( i < len ){
+    		ans = ans * 10 + (str[i] - '0');
+    		i++;
+    	}
+
+    	if(op == '+' && (x + y) == ans)
+    		cnt++;
+    	else if( op == '-' && (x-y) == ans)
+    		cnt++;
+    }
+    cout << cnt << "\n";
+
+    return 0;
+}
