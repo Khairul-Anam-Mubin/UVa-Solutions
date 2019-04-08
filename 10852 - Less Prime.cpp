8039d1bffa639e423847cdc40eeb7c958eab8e98
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-04-08 23:43:48 +0600

    Create 10852 - Less Prime.cpp

---
+++ 10852 - Less Prime.cpp
@@ -0,0 +1,93 @@
+/***
+**     Author: Khairul Anam Mubin
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm>
+
+#define pb      push_back
+#define Sqr(n)  (n*n)
+#define Sort(v) sort(v.begin(),v.end())
+#define Mxe(v)  *max_element(v.begin(),v.end())
+#define Mne(v)  *min_element(v.begin(),v.end())
+#define Fin     freopen("input.txt","r",stdin)
+#define Fout    freopen("output.txt","w",stdout)
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long int li;
+const double pi = acos(-1.0);
+
+template <typename T> T Abs(T a) {if(a<0)return -a;else return a;}
+template <typename T> T BigMod (T b,T p,T m){if (p == 0) return 1;if (p%2 == 0){T s = BigMod(b,p/2,m);return ((s%m)*(s%m))%m;}return ((b%m)*(BigMod(b,p-1,m)%m))%m;}
+template <typename T> T Pow(T B,T P){ if(P==0) return 1; if(P&1) return B*Pow(B,P-1);  else return Sqr(Pow(B,P/2));}
+template <typename T> T Gcd(T a,T b){if(a<0)return Gcd(-a,b);if(b<0)return Gcd(a,-b);return (b==0)?a:Gcd(b,a%b);}
+template <typename T> T Lcm(T a,T b) {if(a<0)return Lcm(-a,b);if(b<0)return Lcm(a,-b);return a*(b/Gcd(a,b));}
+
+char uplowch(char ch){if(ch >= 'A' &&  ch <= 'Z') ch += 32; return ch;}
+char lowupch(char ch){if(ch >= 'a' &&  ch <= 'z') ch -= 32; return ch;}
+string intostr(int x){stringstream ss; ss << x; string str = ss.str(); return str;}
+
+#define MOD 1000000007
+#define MAX -1000000007
+#define MIN 1000000007
+
+//Code starts from here......
+
+#define mx 10000
+bool ar[mx+10];
+vector <int> prime;
+
+void sieve()
+{
+    ar[1] = true;
+    for(int i = 4 ; i <= mx ; i += 2)
+        ar[i] = true;
+
+    for(int i = 3 ; i*i <= mx ; i += 2)
+        if(ar[i] == false)
+            for(int j = i * i ; j <= mx ; j += (i+i))
+                ar[j] = true;
+
+    prime.pb(2);
+    for(int i = 3 ; i <= mx ; i += 2)
+        if(ar[i] == false)
+            prime.pb(i);
+}
+
+int main()
+{
+    // File input and outputs :     Fin ; Fout ;
+
+    sieve();
+    int tc ;
+    scanf("%d",&tc);
+
+    while(tc--)
+    {
+        int n;
+        scanf("%d",&n);
+        int siz = prime.size();
+        for(int i = 0 ; i < siz ; i++){
+            if(prime[i] < n && 2*prime[i] > n){
+                printf("%d\n",prime[i]);
+                break;
+            }
+        }
+    }
+    return 0;
+}
