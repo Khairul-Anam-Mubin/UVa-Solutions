0f7789f04d09d783a66cde715d38a34f61e62754
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-04-18 12:41:18 +0600

    Create 568 - Just the Facts.cpp

---
+++ 568 - Just the Facts.cpp
@@ -0,0 +1,79 @@
+/***
+**     Author: Khairul Anam Mubin
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+
+#define pb      push_back
+#define Sqr(n)  (n*n)
+#define Sort(v) sort(v.begin(),v.end())
+#define Mxe(v)  *max_element(v.begin(),v.end())
+#define Mne(v)  *min_element(v.begin(),v.end())
+#define Fin     freopen("input.txt","r",stdin)
+#define Fout    freopen("output.txt","w",stdout)
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+const double pi = acos(-1.0);
+
+template <typename T> T Abs(T a) {if(a<0)return -a;else return a;}
+template <typename T> T BigMod (T b,T p,T m){if (p == 0) return 1;if (p%2 == 0){T s = BigMod(b,p/2,m);return ((s%m)*(s%m))%m;}return ((b%m)*(BigMod(b,p-1,m)%m))%m;}
+template <typename T> T Pow(T B,T P){ if(P==0) return 1; if(P&1) return B*Pow(B,P-1);  else return Sqr(Pow(B,P/2));}
+template <typename T> T gcd(T a,T b){if(a<0)return gcd(-a,b);if(b<0)return gcd(a,-b);return (b==0)?a:gcd(b,a%b);}
+template <typename T> T lcm(T a,T b) {if(a<0)return lcm(-a,b);if(b<0)return lcm(a,-b);return a*(b/gcd(a,b));}
+
+char uplowch(char ch){if(ch >= 'A' &&  ch <= 'Z') ch += 32; return ch;}
+char lowupch(char ch){if(ch >= 'a' &&  ch <= 'z') ch -= 32; return ch;}
+bool isalpha(char ch){ if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) return true; return false;}
+string intostr(int x){stringstream ss; ss << x; string str = ss.str(); return str;}
+
+#define MOD 1000000007
+#define MAX -1000000007
+#define MIN 1000000007
+
+//Code starts from here......
+
+int dp[10010];
+void fact()
+{
+    dp[0] = 1;
+    dp[1] = 1;
+    int s = 1;
+    for(int i = 2 ; i <= 10000 ; i++){
+        s = s * i;
+        //Applying modular arithmetic theorem..
+        while(s % 10 == 0)
+            s /= 10;
+        s = s % 100000;
+        dp[i] = s % 10;
+    }
+}
+int main()
+{
+    fact();
+
+    int n ;
+    while(cin >> n)
+    {
+        printf("%5d -> %d\n",n,dp[n]);
+    }
+
+    return 0;
+}
