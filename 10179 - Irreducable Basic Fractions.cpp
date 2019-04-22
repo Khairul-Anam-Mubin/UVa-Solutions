e160ce7b5604d3fd4437f878812f95dc27500cab
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-04-22 16:46:29 +0600

    Update 10179 - Irreducable Basic Fractions.cpp

--- 10179 - Irreducable Basic Fractions.cpp
+++ 10179 - Irreducable Basic Fractions.cpp
@@ -19,13 +19,13 @@
 #include <bitset>
 #include <algorithm>
 
-#define pb      push_back
-#define Sqr(n)  (n*n)
-#define Sort(v) sort(v.begin(),v.end())
-#define Mxe(v)  *max_element(v.begin(),v.end())
-#define Mne(v)  *min_element(v.begin(),v.end())
-#define Fin     freopen("input.txt","r",stdin)
-#define Fout    freopen("output.txt","w",stdout)
+#define pb       push_back
+#define Sqr(n)   (n*n)
+#define Sort(v)  sort(v.begin(),v.end())
+#define Mxe(v)   *max_element(v.begin(),v.end())
+#define Mne(v)   *min_element(v.begin(),v.end())
+#define Fin      freopen("input.txt","r",stdin)
+#define Fout     freopen("output.txt","w",stdout)
 
 using namespace std;
 
@@ -38,11 +38,14 @@ template <typename T> T BigMod (T b,T p,T m){if (p == 0) return 1;if (p%2 == 0){
 template <typename T> T Pow(T B,T P){ if(P==0) return 1; if(P&1) return B*Pow(B,P-1);  else return Sqr(Pow(B,P/2));}
 template <typename T> T gcd(T a,T b){if(a<0)return gcd(-a,b);if(b<0)return gcd(a,-b);return (b==0)?a:gcd(b,a%b);}
 template <typename T> T lcm(T a,T b) {if(a<0)return lcm(-a,b);if(b<0)return lcm(a,-b);return a*(b/gcd(a,b));}
+template <typename T> T exgcd(T a,T b,T &x,T &y) {if(a<0){T d=exgcd(-a,b,x,y);x=-x;return d;}   if(b<0){T d=exgcd(a,-b,x,y);y=-y;return d;}   if(b==0){x=1;y=0;return a;}else{T d=exgcd(b,a%b,x,y);T t=x;x=y;y=t-(a/b)*y;return d;}}
 
 char uplowch(char ch){if(ch >= 'A' &&  ch <= 'Z') ch += 32; return ch;}
 char lowupch(char ch){if(ch >= 'a' &&  ch <= 'z') ch -= 32; return ch;}
-bool isalpha(char ch){ if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) return true; return false;}
+bool isalpha(char ch){if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) return true; return false;}
+int strtoint(string str){stringstream ss(str);int x = 0;ss >> x ;return x ;}
 string intostr(int x){stringstream ss; ss << x; string str = ss.str(); return str;}
+vector<string> linetostr(string str){string s; vector <string> v ;istringstream is(str); while(is >> s)v.pb(s);return v;}
 
 #define MOD 1000000007
 #define MAX -1000000007
@@ -50,15 +53,35 @@ string intostr(int x){stringstream ss; ss << x; string str = ss.str(); return st
 
 //Code starts from here......
 
-ll totient(ll x)
-{
-    ll coprime = x;
+#define mx 31625
+bool isp[mx+10];
+vector <ll> prime;
 
-    for(ll i  = 2 ; i*i <= x ; i++){
-        if(x % i == 0){
-            while(x % i == 0)
-                x /= i;
-            coprime -= (coprime/i);
+void sieve()
+{
+    isp[1] = true;
+    for(ll i = 4 ; i <= mx ; i += 2)
+        isp[i] = true;
+    for(ll i = 3 ; i*i <= mx ; i += 2)
+        if(isp[i] == false)
+            for(ll j = i*i ; j <= mx ; j += (i+i))
+                isp[j] = true;
+    prime.pb(2);
+    for(ll i = 3 ; i <= mx ; i += 2)
+        if(isp[i] == false)
+            prime.pb(i);
+}
+
+ll phi(ll x)
+{
+    ll coprime = x ;
+    ll sz = prime.size() ;
+    ll sq = (ll)sqrt(x);
+    for(ll i = 0 ; i < sz && sq >= prime[i] ; i++){
+        if( x % prime[i] == 0){
+            while(x % prime[i] == 0)
+                x /= prime[i];
+            coprime -= (coprime/prime[i]);
         }
     }
     if(x > 1)
@@ -68,11 +91,12 @@ ll totient(ll x)
 
 int main()
 {
+    //file input and output Fin , Fout ;
+    sieve();
     ll n ;
-    while(scanf("%lld",&n) == 1 && n)
+    while(scanf("%lld",&n)==1 && n)
     {
-        printf("%lld\n",totient(n));
+        printf("%lld\n",phi(n));
     }
-
     return 0;
 }
