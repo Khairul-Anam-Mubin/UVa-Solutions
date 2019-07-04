c22219e3206c02d4d5234478e3611dc2e8aae655
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-07-04 18:24:36 +0600

    Update 10801 - Lift Hopping.cpp
    Dijkstra , Graph , (Hard)

--- 10801 - Lift Hopping.cpp
+++ 10801 - Lift Hopping.cpp
@@ -3,7 +3,7 @@
 **     Bangladesh University of Business and Technology,
 **     Dept. of CSE.
 ***/
-#include <bits/stdc++.h>
+
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
@@ -53,6 +53,8 @@ vector<string> linetostr(string str){string s; vector <string> v ;istringstream 
 #define MOD 1000000007
 #define MAX -1000000007
 #define MIN 1e12 + 7
+#define INF 1e9
+#define IINF 1e18
 
 //int dx[] = {0,0,-1,1,-1,-1,1,1};
 //int dy[] = {1,-1,0,0,-1,1,-1,1};
@@ -61,117 +63,128 @@ vector<string> linetostr(string str){string s; vector <string> v ;istringstream 
 
 struct node{
     int x , cost ;
-    bool operator < (const node& p) const {
+    node(int _x , int _cost) {
+        x = _x ;
+        cost = _cost ;
+    }
+    bool operator < ( const node& p) const {
         return cost > p.cost ;
     }
 };
-
-int tim[5] ;
-vector <int> v[6] ;
+int ww[10] ;
+vector <int> v[10] ;
 vector <node> G[1010] ;
 int dist[1010] ;
-int flag = 105 ;
-int dump[10][1010] ;
-void clr(int lifts) {
-    for(int i = 0 ; i <= lifts ; i++)
+int dump[1010] ;
+int lifts , destination ;
+void clr() {
+    for(int i = 0 ; i < 10 ; i++ )
         v[i].clear() ;
+    for(int i = 0 ; i < 1010 ; i++ )
+        G[i].clear() ;
+    Fill0(dump) ;
     return ;
 }
-void check(vector <int> v1 , vector <int> v2 , int lift1 , int lift2 ) {
-    //cout << lift1 << " = " << lift2 << "\n" ;
-    for(int i = 0 ; i < v1.size() ; i++) {
-        for(int j = 0 ; j < v2.size() ; j++) {
-                //cout << j  <<"access\n";
-            if(v1[i] == v2[j]) {
-                if(v1[i] == 0) {
-                    G[0].pb({0 , 0}) ;
-                }
-                else{
-                    G[v1[i]].pb({flag , 60}) ;
-                    dump[lift2][v2[j]] = flag ;
-                    G[flag].pb({v1[i] , 60}) ;
-                    flag++;
+void makegraph() {
+    map <int, int> mp ;
+    int flag = 110 ;
+    for(int i = 0 ; i < lifts ; i++)
+        for(int j = 0 ; j < v[i].size() ; j++)
+            mp[v[i][j]]++ ;
+    for(int i = 0 ; i < lifts ; i++) {
+        for(int j = 0 ; j < v[i].size() ; j++) {
+            if(mp[v[i][j]] > 1 && v[i][j] != 0 && v[i][j] != destination) {
+                dump[flag] = v[i][j] ;
+                v[i][j] = flag ;
+                flag++;
+            }
+        }
+    }
+    for(int i = 0 ; i < lifts ; i++) {
+        for(int j = 0 ; j < v[i].size() - 1 ; j++) {
+            int a, b, w, x, y;
+            a = v[i][j] ;
+            b = v[i][j+1] ;
+            x = a ;
+            y = b ;
+            if(a > 105)
+                a = dump[a] ;
+            if(b > 105)
+                b = dump[b] ;
+            w = Abs(a-b) * ww[i] ;
+            G[x].pb(node(y, w)) ;
+            G[y].pb(node(x, w)) ;
+        }
+    }
+    for(int i = 0 ; i < lifts - 1 ; i++) {
+        for(int j = i + 1 ; j < lifts ; j++) {
+            for(int k = 0 ; k < v[i].size() ; k++) {
+                for(int b = 0 ; b < v[j].size() ; b++) {
+                    int x = v[i][k] ;
+                    int y = v[j][b] ;
+                    int m = x ;
+                    int n = y ;
+                    if(m > 105)
+                        m = dump[m] ;
+                    if(n > 105)
+                        n = dump[n] ;
+                    if( m == n ) {
+                        int w = 0 ;
+                        if(m != 0)
+                            w = 60 ;
+                        G[x].pb(node( y, w)) ;
+                        G[y].pb(node(x, w)) ;
+                    }
                 }
             }
         }
     }
     return ;
 }
-void dijkstra(int nodes ,int from ,int to ) {
-    for(int i = 0 ; i <= nodes + 1 ; i++)
-        dist[i] = 1e9 ;
+void dijkstra(int from , int to) {
+    for(int i = 0 ; i <= 1000 ; i++)
+        dist[i] = INF ;
     priority_queue <node> pq ;
-    pq.push({from , 0});
-    dist[from] = 0;
+    dist[from] = 0 ;
+    pq.push(node(from , 0)) ;
     while(!pq.empty()) {
         int u = pq.top().x ;
         int wu = pq.top().cost ;
         pq.pop() ;
-        if(u == to) {
-            return ;
-        }
+        if(u == to)
+            return;
         if(wu > dist[u])
             continue ;
         for(int i = 0 ; i < G[u].size() ; i++) {
             int v = G[u][i].x ;
             int wv = G[u][i].cost ;
-            if(wu + wv < dist[v]) {
+            if(wu + wv < dist[v]){
                 dist[v] = wu + wv ;
-                pq.push({v , dist[v]}) ;
+                pq.push(node(v , dist[v])) ;
             }
         }
     }
     return ;
 }
 int main() {
-    int lifts , destination ;
-    while(scanf("%d%d",&lifts,&destination) == 2 ) {
-        clr(lifts) ;
-        Fill0(dump) ;
+    while(scanf("%d%d",&lifts,&destination) == 2) {
+        clr() ;
         for(int i = 0 ; i < lifts ; i++)
-            scanf("%d",&tim[i]) ;
+            scanf("%d",&ww[i]) ;
         getchar() ;
-        vector <string> temp ;
         for(int i = 0 ; i < lifts ; i++ ) {
             string str ;
             getline(cin , str) ;
-            temp = linetostr(str) ;
+            vector <string> temp = linetostr(str) ;
             for(int j = 0 ; j < temp.size() ; j++)
                 v[i].pb(strtoint(temp[j])) ;
-            temp.clear() ;
         }
-       // cout << "ok\n" <<"\n";
-        for(int i = 0 ; i < lifts - 1 ; i++) {
-            for(int j = i+1 ; j < lifts ; j++) {
-                check(v[i] , v[j], i , j) ;
-            }
-        }
-        //cout << "ok22\n" ;
-
-        for(int i = 0 ; i < lifts ; i++) {
-            for(int j = 0 ; j < v[i].size() - 1 ; j++) {
-                int now = Abs(v[i][j] - v[i][j+1]) ;
-                now *= tim[i] ;
-                int x = v[i][j] ;
-                int y = v[i][j+1] ;
-                if(dump[i][x] != 0) {
-                    x = dump[i][x] ;
-                }
-                if(dump[i][y] != 0) {
-                    y = dump[i][y] ;
-                }
-                G[x].pb({y , now}) ;
-                G[y].pb({x , now}) ;
-            }
-        }
-        dijkstra(1001 , 0 , destination) ;
-        if(dist[destination] == 1e9)
-            cout << "IMPOSSIBLE\n";
+        makegraph() ;
+        dijkstra(0 , destination) ;
+        if(dist[destination] == INF)
+            printf("IMPOSSIBLE\n");
         else
-            cout << dist[destination] << "\n";
-        for(int i = 0 ; i <= 1000 ; i++) {
-            G[i].clear() ;
-        }
+            printf("%d\n",dist[destination]) ;
     }
-    return 0;
+    return 0 ;
 }
