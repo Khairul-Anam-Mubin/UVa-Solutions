01887082624a745b34ca215209be5c2fe3f71905
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-07-06 16:56:54 +0600

    Create 11492 - Babel.cpp
    There must have some problem in Uva OJ for this solution

---
+++ 11492 - Babel.cpp
@@ -0,0 +1,161 @@
+/***
+**     Author: Khairul Anam Mubin
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+
+#define pb       push_back
+#define Sqr(n)   (n*n)
+#define Sort(v)  sort(v.begin(),v.end())
+#define Mxe(v)   *max_element(v.begin(),v.end())
+#define Mne(v)   *min_element(v.begin(),v.end())
+#define Fill0(a) memset(a,0,sizeof(a))
+#define Fin      freopen("input.txt","r",stdin)
+#define Fout     freopen("output.txt","w",stdout)
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+const double pi = acos(-1.0);
+
+template <typename T> T Abs(T a) {if(a<0)return -a;else return a;}
+template <typename T> T BigMod (T b,T p,T m){if (p == 0) return 1;if (p%2 == 0){T s = BigMod(b,p/2,m);return ((s%m)*(s%m))%m;}return ((b%m)*(BigMod(b,p-1,m)%m))%m;}
+template <typename T> T Pow(T B,T P){ if(P==0) return 1; if(P&1) return B*Pow(B,P-1);  else return Sqr(Pow(B,P/2));}
+template <typename T> T gcd(T a,T b){if(a<0)return gcd(-a,b);if(b<0)return gcd(a,-b);return (b==0)?a:gcd(b,a%b);}
+template <typename T> T lcm(T a,T b) {if(a<0)return lcm(-a,b);if(b<0)return lcm(a,-b);return a*(b/gcd(a,b));}
+template <typename T> T exgcd(T a,T b,T &x,T &y) {if(a<0){T d=exgcd(-a,b,x,y);x=-x;return d;}   if(b<0){T d=exgcd(a,-b,x,y);y=-y;return d;}   if(b==0){x=1;y=0;return a;}else{T d=exgcd(b,a%b,x,y);T t=x;x=y;y=t-(a/b)*y;return d;}}
+template <typename T> typename std::vector<T>::iterator insert_sorted (std :: vector<T> & vec, T const& item) {return vec.insert (std::upper_bound( vec.begin(), vec.end(), item ), item);}
+template <typename T> T isperfectsq(T x){ T n = sqrt(x); return ((n * n == x)?1:0);}
+
+char uplowch(char ch){if(ch >= 'A' &&  ch <= 'Z') ch += 32; return ch;}
+char lowupch(char ch){if(ch >= 'a' &&  ch <= 'z') ch -= 32; return ch;}
+bool isalpha(char ch){if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) return true; return false;}
+int strtoint(string str){stringstream ss(str);int x = 0;ss >> x ;return x ;}
+string intostr(int x){stringstream ss; ss << x; string str = ss.str(); return str;}
+vector<string> linetostr(string str){string s; vector <string> v ;istringstream is(str); while(is >> s)v.pb(s);return v;}
+
+#define MOD 1000000007
+#define MAX -1000000007
+#define MIN 1e12 + 7
+#define INF 1e9
+#define IINF 1e18
+
+//int dx[] = {0,0,-1,1,-1,-1,1,1};
+//int dy[] = {1,-1,0,0,-1,1,-1,1};
+
+//Code starts from here....
+
+struct node{
+    int x ;
+    char inletter ;
+    int cost ;
+    node(int _x , char _inletter , int _cost) {
+        x = _x ;
+        inletter = _inletter ;
+        cost = _cost ;
+    }
+    bool operator < ( const node& p) const {
+        return cost > p.cost ;
+    }
+};
+map <string , int > mp ;
+vector <node> G[5001] ;
+int dist[5001] ;
+int indx = 1 ;
+int mapping(string str) {
+    map <string , int > :: iterator it ;
+    it = mp.find(str) ;
+    if(it != mp.end())
+        return (it->second) ;
+    mp[str] = indx++ ;
+    return ( indx - 1 );
+}
+bool dijkstra(string from , string to) {
+    map <string , int > :: iterator it ;
+    it = mp.find(from) ;
+    if(it == mp.end())
+        return false ;
+    it = mp.find(to) ;
+    if(it == mp.end())
+        return false ;
+    for(int i = 0 ; i <= 5000 ; i++)
+        dist[i] = INF ;
+    int f = mapping(from) ;
+    //cout << "from " << f << "\n" ;
+    int t = mapping(to) ;
+    //cout << "TO " << t << "\n" ;
+    priority_queue<node> pq ;
+    dist[f] = 0 ;
+    pq.push(node(f , '*' , 0)) ;
+    while(!pq.empty()) {
+        int u = pq.top().x ;
+        char inu = pq.top().inletter ;
+        int wu = pq.top().cost ;
+        pq.pop() ;
+        if(t == u)
+            return true ;
+        if(dist[u] < wu)
+            continue ;
+        for(int i = 0 ; i < G[u].size() ; i++) {
+            int v = G[u][i].x ;
+            int wv = G[u][i].cost ;
+            char inv = G[u][i].inletter ;
+            if(inv != inu && dist[v] > wu + wv) {
+                dist[v] = wu + wv ;
+                pq.push(node(v , inv , dist[v])) ;
+            }
+        }
+    }
+    return false ;
+}
+int main() {
+    Fin , Fout ;
+    int n  ;
+    while (cin >> n &&  n != 0) {
+        string from , to ;
+        cin >> from >> to ;
+        for(int i = 0 ; i < n ; i++) {
+            string str1 , str2, wt;
+            cin >> str1 >> str2 >> wt ;
+            int u = mapping(str1) ;
+            int v = mapping(str2) ;
+            int w = wt.size() ;
+            char ini = wt[0] ;
+            //cout << u << " " << v << " " << w << " " << ini <<"\n";
+            G[u].pb(node(v , ini , w)) ;
+            G[v].pb(node(u , ini , w)) ;
+        }
+        bool flag = dijkstra(from , to ) ;
+        //cout << "flag = " << flag << "\n" ;
+        if(flag) {
+            if(dist[mapping(to)] == INF)
+                cout << "impossivel\n" ;
+            else
+                cout << dist[mapping(to)] << '\n' ;
+        }
+        else
+            cout << "impossivel \n" ;
+
+        for(int i = 0 ; i <= 5000 ; i++)
+            G[i].clear() ;
+        mp.clear() ;
+        indx = 1 ;
+    }
+    return 0 ;
+}
