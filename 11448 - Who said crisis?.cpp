132f8805041d820d1f92d0cd449dc25f72fe1127
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-10-04 18:14:31 +0600

    Create 11448 - Who said crisis?.cpp

---
+++ 11448 - Who said crisis?.cpp
@@ -0,0 +1,159 @@
+/***
+**     Author: Khairul Anam Mubin
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+
+#define pb       push_back
+#define MP       make_pair
+#define Sort(v)  sort(v.begin(),v.end())
+#define Mxe(v)   *max_element(v.begin(),v.end())
+#define Mne(v)   *min_element(v.begin(),v.end())
+#define Fill0(a) memset(a,0,sizeof(a))
+#define Fin      freopen("input.txt","r",stdin)
+#define Fout     freopen("output.txt","w",stdout)
+#define precision(a) cout << fixed << setprecision(a) 
+#define Fast     fasrerIO()
+
+using namespace std;
+
+template <typename T> T Sqr(T x) { T n = x * x ; return n ;}
+template <typename T> T Abs(T a) {if(a<0)return -a;else return a;}
+template <typename T> T BigMod (T b,T p,T m){if (p == 0) return 1;if (p%2 == 0){T s = BigMod(b,p/2,m);return ((s%m)*(s%m))%m;}return ((b%m)*(BigMod(b,p-1,m)%m))%m;}
+template <typename T> T ModInv (T b,T m){return BigMod(b,m-2,m);}
+template <typename T> T Pow(T B,T P){ if(P==0) return 1; if(P&1) return B*Pow(B,P-1);  else return Sqr(Pow(B,P/2));}
+template <typename T> T gcd(T a,T b){if(a<0)return gcd(-a,b);if(b<0)return gcd(a,-b);return (b==0)?a:gcd(b,a%b);}
+template <typename T> T lcm(T a,T b) {if(a<0)return lcm(-a,b);if(b<0)return lcm(a,-b);return a*(b/gcd(a,b));}
+template <typename T> T exgcd(T a,T b,T &x,T &y) {if(a<0){T d=exgcd(-a,b,x,y);x=-x;return d;}   if(b<0){T d=exgcd(a,-b,x,y);y=-y;return d;}   if(b==0){x=1;y=0;return a;}else{T d=exgcd(b,a%b,x,y);T t=x;x=y;y=t-(a/b)*y;return d;}}
+template <typename T> typename std::vector<T>::iterator insert_sorted (std :: vector<T> & vec, T const& item) {return vec.insert (std::upper_bound( vec.begin(), vec.end(), item ), item);}
+template <typename T> T isperfectsq(T x){ T n = sqrt(x); return ((n * n == x)?1:0);}
+
+
+char uplowch(char ch){if(ch >= 'A' &&  ch <= 'Z') ch += 32; return ch;}
+char lowupch(char ch){if(ch >= 'a' &&  ch <= 'z') ch -= 32; return ch;}
+bool isalpha(char ch){if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) return true; return false;}
+int strtoint(string str){stringstream ss(str);int x = 0;ss >> x ;return x ;}
+string intostr(int x){stringstream ss; ss << x; string str = ss.str(); return str;}
+vector<string> linetostr(string str){string s; vector <string> v ;istringstream is(str); while(is >> s)v.pb(s);return v;}
+void fasrerIO() {ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0) ;}
+
+#define INF 2147483647
+#define IINF 9223372036854775807
+#define WHITE 0 
+#define GRAY 1
+#define BLACK 2
+#define eps 1e-9
+
+typedef unsigned long long ull;
+typedef long long ll;
+const double pi = acos(-1.0);
+
+//int dx[] = {0,0,-1,1,-1,-1,1,1,0};
+//int dy[] = {1,-1,0,0,-1,1,-1,1,0};
+//int dx[] = {0 , 0 , -1 , 1} ; // right , left , for , back
+//int dy[] = {1 , -1 , 0 , 0} ; 
+//scanf(" %[^\n]", str) ;
+//ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0) ;
+
+struct Bigint {
+    // representations and structures
+    string a; // to store the digits
+    int sign; // sign = -1 for negative numbers, sign = 1 otherwise
+    
+    // constructors
+    Bigint() {} // default constructor
+    Bigint( string b ) { (*this) = b; } // constructor for string
+    // some helpful methods
+    int size() { // returns number of digits
+        return a.size();
+    }
+    Bigint inverseSign() { // changes the sign
+        sign *= -1;
+        return (*this);
+    }
+    Bigint normalize( int newSign ) { // removes leading 0, fixes sign
+        for( int i = a.size() - 1; i > 0 && a[i] == '0'; i-- )
+            a.erase(a.begin() + i);
+        sign = ( a.size() == 1 && a[0] == '0' ) ? 1 : newSign;
+        return (*this);
+    }
+
+    // assignment operator
+    void operator = ( string b ) { // assigns a string to Bigint
+        a = b[0] == '-' ? b.substr(1) : b;
+        reverse( a.begin(), a.end() );
+        this->normalize( b[0] == '-' ? -1 : 1 );
+    }
+
+    // conditional operators
+    bool operator < ( const Bigint &b ) const { // less than operator
+        if( sign != b.sign ) return sign < b.sign;
+        if( a.size() != b.a.size() )
+            return sign == 1 ? a.size() < b.a.size() : a.size() > b.a.size();
+        for( int i = a.size() - 1; i >= 0; i-- ) if( a[i] != b.a[i] )
+            return sign == 1 ? a[i] < b.a[i] : a[i] > b.a[i];
+        return false;
+    }
+    bool operator == ( const Bigint &b ) const { // operator for equality
+        return a == b.a && sign == b.sign;
+    }
+    // mathematical operators
+    Bigint operator + ( Bigint b ) { // addition operator overloading
+        if( sign != b.sign ) return (*this) - b.inverseSign();
+        Bigint c;
+        for(int i = 0, carry = 0; i<a.size() || i<b.size() || carry; i++ ) {
+            carry+=(i<a.size() ? a[i]-48 : 0)+(i<b.a.size() ? b.a[i]-48 : 0);
+            c.a += (carry % 10 + 48);
+            carry /= 10;
+        }
+        return c.normalize(sign);
+    }
+    Bigint operator - ( Bigint b ) { // subtraction operator overloading
+        if( sign != b.sign ) return (*this) + b.inverseSign();
+        int s = sign; sign = b.sign = 1;
+        if( (*this) < b ) return ((b - (*this)).inverseSign()).normalize(-s);
+        Bigint c;
+        for( int i = 0, borrow = 0; i < a.size(); i++ ) {
+            borrow = a[i] - borrow - (i < b.size() ? b.a[i] : 48);
+            c.a += borrow >= 0 ? borrow + 48 : borrow + 58;
+            borrow = borrow >= 0 ? 0 : 1;
+        }
+        return c.normalize(s);
+    }
+    // output method
+    void print() {
+        if( sign == -1 ) putchar('-');
+        for( int i = a.size() - 1; i >= 0; i-- ) putchar(a[i]);
+    }
+};
+int main() {
+    int tc ;
+    cin >> tc ;
+    while(tc--) {
+        string a , b ;
+        cin >> a >> b ;
+        Bigint x , y , z ;
+        x = a ;
+        y = b ;
+        z = x - y ;
+        z.print() ;
+        printf("\n") ;
+    }
+    return  0 ;
+}
