8a8fef9305967f944f86dbdf38c9a277bf5c6fd8
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2020-04-02 15:45:37 +0600

    Update 11987 - Almost Union-Find.cpp

--- 11987 - Almost Union-Find.cpp
+++ 11987 - Almost Union-Find.cpp
@@ -8,8 +8,8 @@
  
 #include <bits/stdc++.h>
 using namespace std;
- 
- 
+
+
 #define F            first
 #define S            second  
 #define PB           push_back
@@ -24,12 +24,12 @@ using namespace std;
 //#include <ext/pb_ds/detail/standard_policies.hpp>
 using namespace __gnu_pbds;
 typedef tree < 
-		int, 
-		null_type , 
-		less < int > , 
-		rb_tree_tag , 
-		tree_order_statistics_node_update > 
-		ordered_set ;
+        int, 
+        null_type , 
+        less < int > , 
+        rb_tree_tag , 
+        tree_order_statistics_node_update > 
+        ordered_set ;
 // *st.find_by_order(k) ; kth element
 //  st.order_of_key(k) ; number of element less than k  
 */
@@ -55,7 +55,7 @@ vector<string> Linetostr(string str){string s; vector <string> v ;istringstream 
 typedef unsigned long long ull;
 typedef long long ll;
 const double pi = 2 * acos(0.0);
- 
+
 #define MOD 998244353
 #define INF9 2147483647
 #define INF18 9223372036854775806
@@ -63,88 +63,88 @@ const double pi = 2 * acos(0.0);
 #define GRAY 1
 #define BLACK 2
 #define eps 1e-8
- 
+
 //int dx[] = {1, 1, 1, -1, -1, -1, 0, 0};
 //int dy[] = {0, -1, 1, 0, -1, 1, -1, 1};
- 
+
 //int dx[] = {0 , 0 , -1 , 1} ; // right , left , forward , backward
 //int dy[] = {1 , -1 , 0 , 0} ; 
- 
+
 //scanf(" %[^\n]", str) ;
- 
+
 struct DSU {
-	vector <int> parent , siz;
-	vector <int> sum;
- 
-	DSU(int mxN) {
-		mxN <<= 1;
-		parent.resize(mxN + 1);
-		siz.resize(mxN + 1);
- 
-		sum.resize(mxN + 1);
-	}
-	void Makeset(int n) {
-		for (int i = 1 ; i <= n ; i++) {
-			parent[i] = n + i;
-			parent[n + i] = n + i;
-			siz[n + i] = 1;
-			sum[n + i] = i;
-		}
-	}
-	int Find(int u) {
-		if (parent[u] == u) 
-			return u ;
-		return parent[u] = Find(parent[u]) ;
-	}
-	void Union(int u , int v) {
-		u = Find(u);
-		v = Find(v);
-		if (u != v) {
-			parent[u] = v ;
-			siz[v] += siz[u];
- 
-			sum[v] += sum[u];
-		}
-	}
-	bool SameSet(int u , int v) {
-		return (Find(u) == Find(v)) ;
-	}
-	int Size(int u) {
-		return siz[Find(u)];
-	}
-	int Sum(int u) {
-		return sum[Find(u)];
-	}
-	void Move(int u , int v) {
-		if (SameSet(u , v)) return;
-		int x = Find(u);
-		int y = Find(v);
-		siz[x]--;
-		siz[y]++;
-		sum[x] -= u;
-		sum[y] += u;
-		parent[u] = y;
-	}
+    vector <int> parent , siz;
+    vector <int> sum;
+    
+    DSU(int mxN) {
+        mxN <<= 1;
+        parent.resize(mxN + 1);
+        siz.resize(mxN + 1);
+
+        sum.resize(mxN + 1);
+    }
+    void Makeset(int n) {
+        for (int i = 1 ; i <= n ; i++) {
+            parent[i] = n + i;
+            parent[n + i] = n + i;
+            siz[n + i] = 1;
+            sum[n + i] = i;
+        }
+    }
+    int Find(int u) {
+        if (parent[u] == u) 
+            return u ;
+        return parent[u] = Find(parent[u]) ;
+    }
+    void Union(int u , int v) {
+        u = Find(u);
+        v = Find(v);
+        if (u != v) {
+            parent[u] = v ;
+            siz[v] += siz[u];
+            
+            sum[v] += sum[u];
+        }
+    }
+    bool SameSet(int u , int v) {
+        return (Find(u) == Find(v)) ;
+    }
+    int Size(int u) {
+        return siz[Find(u)];
+    }
+    int Sum(int u) {
+        return sum[Find(u)];
+    }
+    void Move(int u , int v) {
+        if (SameSet(u , v)) return;
+        int x = Find(u);
+        int y = Find(v);
+        siz[x]--;
+        siz[y]++;
+        sum[x] -= u;
+        sum[y] += u;
+        parent[u] = y;
+    }
 };
 int main() {
     FasterIO
-	DSU dsu(100000);
-	int n , m;
-	while (cin >> n >> m) {
-		dsu.Makeset(n);
-		while (m--) {
-			int q; cin >> q;
-			if (q == 1) {
-				int u , v; cin >> u >> v;
-				dsu.Union(u , v);
-			} else if (q == 2) {
-				int u , v; cin >> u >> v;
-				dsu.Move(u , v);
-			} else if (q == 3) {
-				int st; cin >> st;
-				cout << dsu.Size(st) << " " << dsu.Sum(st) << "\n";
-			}
-		}
-	}
-	return 0 ;
+    DSU dsu(100000);
+    int n , m;
+    while (cin >> n >> m) {
+        dsu.Makeset(n);
+        while (m--) {
+            int q; cin >> q;
+            if (q == 1) {
+                int u , v; cin >> u >> v;
+                dsu.Union(u , v);
+            } else if (q == 2) {
+                int u , v; cin >> u >> v;
+                dsu.Move(u , v);
+            } else if (q == 3) {
+                int st; cin >> st;
+                cout << dsu.Size(st) << " " << dsu.Sum(st) << "\n";
+            }
+        }
+    }
+    return 0 ;
 }
